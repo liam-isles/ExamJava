c219ecdc4009965603e1d083eea851ef07ed132e
Liam Isles <liam.isles@academytrainee.com>
2018-05-11 12:05:32 +0100

    maths task

---
+++ maths/src/maths/mathsMain.java
@@ -0,0 +1,13 @@
+package maths;
+import java.math.*;
+public class mathsMain {
+
+	public static void main(String[] args) {
+		sum sum = new sum();
+		sum.add(20, 27);
+		sum.subtract(50, 18);
+		sum.multiply(5, 74);
+		sum.divide(150, 2);
+	}
+
+}
---
+++ maths/src/maths/sum.java
@@ -0,0 +1,23 @@
+package maths;
+
+public class sum {
+
+		public static void add(int a, int b) {
+			int c = a + b;
+			System.out.println(a + " + " + b + " = " + c);		
+		}
+
+		public static void subtract(int a, int b) {
+			int c = a - b;
+			System.out.println(a + " - " + b + " = " + c);		
+}
+		public static void divide(int a, int b) {
+			int c = a / b;
+			System.out.println(a + " divied by " + b + " = " + c);		
+}
+		
+		public static void multiply(int a, int b) {
+			int c = a * b;
+			System.out.println(a + " x " + b + " = " + c);		
+}
+}
